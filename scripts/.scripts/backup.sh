10efeb4204f26574eb5045508f9530a9f1c428ea
´Enrique <ennanco@gmail.com>
2020-11-12 20:40:29 +0100

    Various updates and cleaning due to XDG

--- scripts/.scripts/backup.sh
+++ scripts/.scripts/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-EXCLUDE="--exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/home/lost+found --exclude=/home/*/.cache/* --exclude=/home/*/.local/share/Trash/ --exclude=/home/*/snap"
-BACKUP_FILE="/media/quique/My\\ Passport/_Backup"
+#EXCLUDE="--exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/home/lost+found --exclude=/home/*/.cache/* --exclude=/home/*/.local/share/Trash/ --exclude=/home/*/snap --exclude=swapfile"
+EXCLUDE="--exclude=/home/lost+found --exclude=/home/*/.cache/* --exclude=/home/*/.local/share/Trash/ --exclude=/home/*/snap"
+BACKUP_FILE="/media/quique/Trabajo/_Backup"
 
-sudo rsync -AaHXSvn --delete ${EXCLUDE} / ${BACKUP_FILE}
+sudo rsync -AaHXSv --delete ${EXCLUDE} /home ${BACKUP_FILE}
